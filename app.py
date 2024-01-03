c7cb4c1d1fe1247578c0222489bd826159f8abe3
jane mckenna <janemckenna@hotmail.com>
2024-01-03 17:39:56 +0000

    Feat: add profile and registration pages

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -22,6 +22,56 @@ mongo = PyMongo(app)
 def blog_posts():
     blogs = list(mongo.db.blogs.find())
     return render_template("blogs.html", blogs=blogs)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if username/email already exist
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        existing_email = mongo.db.users.find_one(
+            {"email": request.form.get("email").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        if existing_email:
+            flash("Email already in use")
+            return redirect(url_for("register"))
+
+        password = request.form.get("password")
+        confirm = request.form.get("confirm")
+        
+        if password == confirm:
+            register = {
+                "email": request.form.get("email").lower(),
+                "username": request.form.get("username").lower(),
+                "password": generate_password_hash(request.form.get("password")),
+                "is_admin": "No"
+            }
+            mongo.db.users.insert_one(register)
+
+            # put the new user into 'session' cookie
+            session['user'] = request.form.get("username").lower()
+            flash("Registration successful!")
+            return redirect(url_for("profile", username=session["user"]))
+
+    return render_template("register.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grab the session users username from the database
+    username =mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
 
 
 # NOTE TO SELF: UPDATE TO DEBUG=False prior to submitting
--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
     $(".collapsible").collapsible();
+    // check passwords match
+    $('#password, #confirm').on('keyup', function () {
+        if ($('#password').val() == $('#confirm').val()) {
+          $('#password-message').html('Passwords Match').css('color', 'green');
+        } else 
+          $('#password-message').html('Passwords Do Not Match').css('color', 'red');
+      });
     });
+
